ff54d175ba239dd05acc00ea1af603d5341a1fd0
daralapavankumar1184 <daralapavan@gmail.com>
2023-03-27 19:36:46 +0530

    loginbackup

---
+++ loginapp/loginapp/src/main/java/com/Login/util/EncodePassword.java
@@ -0,0 +1,11 @@
+package com.Login.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class EncodePassword {
+    public static void main(String[] args) {
+        PasswordEncoder encodePassword = new BCryptPasswordEncoder();
+        System.out.println(encodePassword.encode("password"));
+    }
+}
